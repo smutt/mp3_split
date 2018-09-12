63f8a94f0ff4de2fc3026757918cf95146890536
smutt <smutt@depht.com>
2018-09-12 14:22:30 +0000

    hackin

--- split.py
+++ split.py
@@ -6,15 +6,85 @@ import signal
 import subprocess
 import argparse
 
+
 ###########
 # GLOBALS #
 ###########
-
 FFMPEG = "/usr/local/bin/ffmpeg"
+
+
+###########
+# CLASSES #
+###########
+class Chapter():
+  def __init__(self, start, end):
+    self.title = ""
+    self.start = start
+    self.end = end
+
+  def __repr__(self):
+    return "<title:" + self.title + " start:" + str(self.start) + " end:" + str(self.end) + ">"
+
 
 #############
 # FUNCTIONS #
 #############
+# Count how many of char c begin in s before another char
+# deprecated
+def charCnt(s, c):
+  if s.find(c) == 0:
+    return 1 + charCnt(s[1:], c)
+  else:
+    return 0
+
+# Takes output from ffmpeg info cmd
+# Returns parsed info
+def parseInfo(ss):
+  lines = ss.splitlines()
+
+  for ii in xrange(len(lines)):
+    if lines[ii].find("Input #") == 0:
+      start = ii
+    if lines[ii].find("Output #") == 0:
+      end = ii
+  lines = lines[start+1:end]
+
+  rv = {}
+  rv["general"] = {}
+  rv["metadata"] = {}
+  rv["chapters"] = []
+  active = ""
+
+  for ll in lines:
+    if ll.find("  Metadata:") == 0:
+      active = "metadata"
+      continue
+
+    elif ll.find("  Duration:") == 0:
+        vals = ll.strip().split(",")
+        rv["general"]["duration"] = vals[0].split("Duration: ")[1]
+        rv["general"]["start"] = float(vals[1].split("start: ")[1].strip())
+        rv["general"]["bitrate"] = int(vals[2].strip().split(" ")[1])
+        continue
+
+    elif ll.find("    Chapter #") == 0:
+      active = "chapters"
+      vals = ll.split(",")
+      start = float(vals[0].split("start ")[1].strip())
+      end = float(vals[1].split("end ")[1].strip())
+      rv["chapters"].append(Chapter(start, end))
+      continue
+
+    if active == "metadata":
+      vals = ll.strip().split(":", 1)
+      rv["metadata"][vals[0].strip()] = vals[1].strip()
+
+    elif active == "chapters":
+      if ll.strip().find("title") == 0:
+        rv["chapters"][-1].title = ll.split(":", 1)[1].strip()
+
+  return rv
+
 
 # Call ffmpeg binary and returns output
 def ff(cmd):
@@ -34,7 +104,6 @@ def euthanize(signal, frame):
 ###################
 # BEGIN EXECUTION #
 ###################
-
 signal.signal(signal.SIGINT, euthanize)
 signal.signal(signal.SIGTERM, euthanize)
 signal.signal(signal.SIGABRT, euthanize)
@@ -59,13 +128,22 @@ args = ap.parse_args()
 
 infile = args.infile[0]
 
-# Load the info on the file
-info = ff("-i " + infile + " -f null -")
+# Capture info on our file
+info = parseInfo(ff("-i " + infile + " -f null -"))
 
-if arg.dump:
-  print info
+if args.dump:
+  print("__general__")
+  for k,v in info["general"].iteritems():
+    print(k + " : " + str(v))
+
+  print("__metadata__")
+  for k,v in info["metadata"].iteritems():
+    print(k + " : " + str(v))
+
+  print("__chapters__")
+  for chap in info["chapters"]:
+    print(repr(chap))
   exit(0)
-
 
 
 # Some handy commands
